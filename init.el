56355fdc2b872d52a48f02341da277c53fdeeb1d
Puda <puda.adup@yahoo.co.uk>
2016-04-07 22:47:49 -0400

    Changes

--- init.el
+++ init.el
@@ -95,9 +95,8 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         spacemacs-dark
-                         material
                          gruvbox
+                         material
                          )
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
