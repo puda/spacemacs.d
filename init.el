259294fc89a7e537c2145213cc123326145003e7
Puda <puda.adup@yahoo.co.uk>
2016-04-29 22:07:58 -0400

    Changes

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
    dotspacemacs-startup-lists '(recents bookmarks projects)
    dotspacemacs-startup-recent-list-size 5
    dotspacemacs-scratch-mode 'text-mode
-   dotspacemacs-themes '(spacemacs-dark material monokai solarized-dark)
+   dotspacemacs-themes '(monokai spacemacs-dark solarized-dark)
    dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Source Code Pro" :size 38 :weight semibold :width normal :powerline-scale 1.1)
    dotspacemacs-leader-key "SPC"
