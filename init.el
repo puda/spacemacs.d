ddf06ae9c3251c2c5987f5ae0f0645ca04035eca
Puda <puda.adup@yahoo.co.uk>
2016-05-01 23:57:46 -0400

    changes

--- init.el
+++ init.el
@@ -81,7 +81,7 @@
    dotspacemacs-inactive-transparency 90
    dotspacemacs-show-transient-state-title nil
    dotspacemacs-show-transient-state-color-guide nil
-   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-mode-line-unicode-symbols nil
    dotspacemacs-smooth-scrolling nil
    dotspacemacs-line-numbers 'relative
    dotspacemacs-smartparens-strict-mode nil
