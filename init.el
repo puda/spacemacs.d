fcf10472011c280f5baf112f2c1f0600ffb43b38
Puda <puda.adup@yahoo.co.uk>
2016-05-04 21:58:37 -0400

    Changes

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
      )
 
    dotspacemacs-additional-packages '()
-   dotspacemacs-excluded-packages '(drupal-mode vi-tilde-fringe evil-search-highlight-persist spaceline)
+   dotspacemacs-excluded-packages '(drupal-mode vi-tilde-fringe evil-search-highlight-persist)
    ))
 
 
@@ -59,7 +59,7 @@
    dotspacemacs-scratch-mode 'text-mode
    dotspacemacs-themes '(monokai material leuven cyberpunk spacemacs-dark solarized-dark)
    dotspacemacs-colorize-cursor-according-to-state t
-   dotspacemacs-default-font '("Source Code Pro" :size 38 :weight semibold :width normal :powerline-scale 1.15)
+   dotspacemacs-default-font '("Source Code Pro" :size 38 :weight semibold :width normal :powerline-scale 1.1)
    dotspacemacs-leader-key "SPC"
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
@@ -112,9 +112,6 @@
    ;; Spaceline
    spaceline-buffer-encoding-abbrev-p nil
    spaceline-version-control-p nil
-
-   ;; Helm Swoop
-   helm-swoop-speed-or-color nil
    ;; General
    ;; default-directory "/var/www/community/"
    ;; initial-scratch-message ";; Puda loves Rachael!! \n;; Puda Emacs Custom Config!!"
@@ -139,6 +136,8 @@
    evil-snipe-spillover-scope 'visible
    ;; Spaceline
    powerline-default-separator 'arrow
+   ;; Helm Swoop
+   helm-swoop-speed-or-color nil
    )
 
   ;; Modes
@@ -175,3 +174,76 @@
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("51277c9add74612c7624a276e1ee3c7d89b2f38b1609eed6759965f9d4254369" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-annotation ((t (:foreground "#ff9eb8"))))
+ '(company-tooltip-annotation-selection ((t (:background "#66d9ef"))))
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ '(erc-timestamp-face ((t (:inherit font-lock-comment-face :foreground nil))))
+ '(evil-search-highlight-persist-highlight-face ((t (:background "#fc5fef" :foreground "#000000"))))
+ '(flycheck-fringe-error ((t (:background nil))))
+ '(flycheck-fringe-info ((t (:background nil))))
+ '(flycheck-fringe-warning ((t (:background nil))))
+ '(font-latex-sectioning-0-face ((t (:inherit default :weight bold))))
+ '(font-latex-sectioning-1-face ((t (:inherit default :weight bold))))
+ '(font-latex-sectioning-2-face ((t (:inherit default :weight bold))))
+ '(font-latex-sectioning-3-face ((t (:inherit default :weight bold))))
+ '(font-latex-sectioning-4-face ((t (:inherit default :weight bold))))
+ '(font-latex-sectioning-5-face ((t (:inherit default :weight bold))))
+ '(font-latex-slide-title-face ((t (:inherit default :weight bold))))
+ '(font-lock-builtin-face ((t (:foreground "#ff9eb8" :weight semi-bold))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-doc-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:weight bold))))
+ '(font-lock-string-face ((t (:slant italic))))
+ '(font-lock-warning-face ((t (:underline nil))))
+ '(header-line ((t (:box (:color "#555555" :line-width 1)))))
+ '(helm-ff-prefix ((t (:background nil :foreground "#666666" :weight bold))))
+ '(helm-prefarg ((t (:foreground "PaleGreen"))))
+ '(info-title-1 ((t (:inherit default :weight bold))))
+ '(info-title-2 ((t (:inherit default :weight bold))))
+ '(info-title-3 ((t (:inherit default :weight bold))))
+ '(info-title-4 ((t (:inherit default :weight bold))))
+ '(markdown-header-face ((t (:inherit default :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit default :weight bold))))
+ '(markdown-header-face-2 ((t (:inherit default :weight bold))))
+ '(markdown-header-face-3 ((t (:inherit default :weight bold))))
+ '(markdown-header-face-4 ((t (:inherit default :weight bold))))
+ '(markdown-header-face-5 ((t (:inherit default :weight bold))))
+ '(markdown-header-face-6 ((t (:inherit default :weight bold))))
+ '(mode-line ((t (:box (:color "#999999" :line-width 1 :style released-button)))))
+ '(mode-line-inactive ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(org-document-title ((t (:inherit default :weight bold))))
+ '(org-done ((t (:foreground "MediumSpringGreen"))))
+ '(org-level-1 ((t (:inherit default :weight bold))))
+ '(org-level-2 ((t (:inherit default :weight bold))))
+ '(org-level-3 ((t (:inherit default :weight bold))))
+ '(org-level-4 ((t (:inherit default :weight bold))))
+ '(org-level-5 ((t (:inherit default :weight bold))))
+ '(org-level-6 ((t (:inherit default :weight bold))))
+ '(org-level-7 ((t (:inherit default :weight bold))))
+ '(org-level-8 ((t (:inherit default :weight bold))))
+ '(powerline-active1 ((t (:box (:color "#999999" :line-width 1 :style released-button) :background "#5a5a5a"))))
+ '(powerline-active2 ((t (:box (:color "#999999" :line-width 1 :style released-button)))))
+ '(powerline-inactive1 ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(powerline-inactive2 ((t (:box (:color "#666666" :line-width 1 :style released-button)))))
+ '(region ((t (:background "#998f84"))))
+ '(spacemacs-transient-state-title-face ((t (:background nil :foreground nil :inherit font-lock-warning-face))))
+ '(term ((t (:foreground nil :background nil))))
+ '(web-mode-comment-face ((t (:inherit font-lock-comment-face :foreground nil))))
+ '(web-mode-html-attr-name-face ((t (:inherit font-lock-variable-name-face :foreground nil))))
+ '(web-mode-html-attr-value-face ((t (:inherit font-lock-string-face :foreground nil))))
+ '(web-mode-html-tag-bracket-face ((t (:inherit web-mode-html-tag-face :foreground nil))))
+ '(web-mode-html-tag-face ((t (:inherit font-lock-builtin-face :foreground nil :weight bold)))))
