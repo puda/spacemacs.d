e234ebbd903c41e861fe26e0c004f25e58271106
Puda <puda.adup@yahoo.co.uk>
2016-06-03 00:16:44 -0400

    Changes

--- layers/puda-theming/config.el
+++ layers/puda-theming/config.el
@@ -2,7 +2,7 @@
  theming-modifications
  '((;; monokai
     ;; Font locking
-    hc-zenburn
+    darkokai
     (font-lock-comment-face :slant italic)
     (font-lock-string-face :slant italic)
     (font-lock-doc-face :slant italic)
