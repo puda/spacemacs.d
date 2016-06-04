2debf1a40fc79f9038ab28d04b872b7e7ec1117a
Puda <puda.adup@yahoo.co.uk>
2016-06-04 18:40:30 -0400

    Changes

--- init.el
+++ init.el
@@ -70,7 +70,7 @@
    dotspacemacs-scratch-mode 'text-mode
    dotspacemacs-themes '(darkokai monokai solarized-dark material spacemacs-dark)
    dotspacemacs-colorize-cursor-according-to-state t
-   dotspacemacs-default-font '("Source Code Pro" :size 38 :weight demibold :width normal :powerline-scale 1.15)
+   dotspacemacs-default-font '("Source Code Pro" :size 38 :weight semibold :width normal :powerline-scale 1.15)
    dotspacemacs-leader-key "SPC"
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
